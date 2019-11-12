28dadea4ba621bdc061be56015e7025606c05c66
YongMinSun <ddoni1010@gmail.com>
2019-11-12 19:35:49 +0900

    Initial commit

---
+++ app/src/main/java/com/tioeun/a20191112_01_fragment/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.tioeun.a20191112_01_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract  class BaseActivity : AppCompatActivity() {
+
+    abstract fun setupEvents()
+
+    abstract fun setValues()
+}
