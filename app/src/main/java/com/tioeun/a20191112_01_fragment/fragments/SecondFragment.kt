dc415d6fa859487a195fbf731ec6de1a91d3616a
YongMinSun <ddoni1010@gmail.com>
2019-11-12 20:13:29 +0900

    두번쨰 프로그먼트 연습문제

--- app/src/main/java/com/tioeun/a20191112_01_fragment/fragments/SecondFragment.kt
+++ app/src/main/java/com/tioeun/a20191112_01_fragment/fragments/SecondFragment.kt
@@ -9,21 +9,19 @@ import com.tioeun.a20191112_01_fragment.R
 import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.fragment_second.*
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
-//    액티비티 위에 얹어졌을떄 실행되는 fun
-//    실질적으로 fragment 입장에서는 onCreate나 마찬가지.
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        firstFragTxt.text = "프래그먼트에서 글자 바꿈"
+        secondFragTxt.text = "두번쨰 프로그먼트에서 글자 바꿈"
     }
 }
