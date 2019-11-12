2730b9db66076bf0259da57096ee1c4fa9d36ec0
YongMinSun <ddoni1010@gmail.com>
2019-11-12 20:35:03 +0900

    가로스크롤뷰 예제  - 페이징 처리를 하고 싶다.

---
+++ app/src/main/java/com/tioeun/a20191112_01_fragment/TestActivity.kt
@@ -0,0 +1,23 @@
+package com.tioeun.a20191112_01_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class TestActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
